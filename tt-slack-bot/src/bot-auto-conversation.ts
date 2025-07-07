190bbe0144df8e2911873169a91d84484bd2632d
George Tao <yultao@gmail.com>
2025-07-07 13:06:50 -0400

    Remove tt-slack-bot module and associated files

--- tt-slack-bot/src/bot-auto-conversation.ts
+++
@@ -1,4 +0,0 @@
-
-import Bot  from 'tt-ai-agent/Bot';
-const bot = new Bot("C:/Workspace/ai/tt-ai-agent/aiconfig.json");
-await bot.autoConversation();
--- tt-slack-bot/src/bot-single.ts
+++
@@ -1,5 +0,0 @@
-// import {logInfo}  from 'tt-ai-agent/logInfo';
-// logInfo("Starting tt-ai-provider...");
-import Bot  from 'tt-ai-agent/Bot';
-const bot = new Bot("C:/Workspace/ai/tt-ai-agent/aiconfig.json");
-console.log(await bot.askSingleQuestion('What is the capital of France?'));
